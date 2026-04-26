6b924f9d9e3edb532fd147125546339c81ccbf11
ramon-bryant2 <ramon.bryant@bison.howard.edu>
2026-04-26 16:29:20 -0400

    Update mycode.cpp

--- mycode.cpp
+++ mycode.cpp
@@ -15,3 +15,14 @@ int main(int argc, char* argv[]) {
  string encoded = "";
  for (char c : message) {
     if (isalpha(c)) {
+        char upper = toupper(c);
+        char shifted = (char)(((upper - 'A' + shift) % 26) + 'A');
+        encoded += shifted;
+        }
+    }
+ int len = encoded.length();
+    int pos = 0;
+    int blocksOnLine = 0;
+    while (pos < len) {
+        string block = encoded.substr(pos, 5);
+        cout << block;
